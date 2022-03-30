1b6e0d3eabec5ec447d93ccc8dd3deb4f4818705
Elia <frodohack@gmail.com>
2022-03-30 10:43:42 +1000

    Updates

--- resources/public/js/cljs-runtime/dda.defs.js
+++
@@ -1,4 +0,0 @@
-goog.provide('dda.defs');
-dda.defs.block_colors = new cljs.core.PersistentVector(null, 7, 5, cljs.core.PersistentVector.EMPTY_NODE, ["bg-slate-700","bg-slate-600","bg-slate-500","bg-slate-400","bg-slate-300","bg-slate-200","bg-slate-100"], null);
-
-//# sourceMappingURL=dda.defs.js.map
--- resources/public/js/cljs-runtime/re_frisk_remote.preload.js
+++
@@ -1,4 +0,0 @@
-goog.provide('re_frisk_remote.preload');
-re_frisk_remote.core.enable();
-
-//# sourceMappingURL=re_frisk_remote.preload.js.map
